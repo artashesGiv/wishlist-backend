c581ded8ea95e6fe95a4a39c72fb50cb4b6a60ee
Artashes <artashes.giv@gmail.com>
2024-08-14 22:59:12 +0300

    add pre-commit script

--- src/wishes/wishes.controller.ts
+++ src/wishes/wishes.controller.ts
@@ -1,11 +1,11 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
 } from '@nestjs/common'
 import { WishesService } from './wishes.service'
 import { CreateWishDto } from './dto/create-wish.dto'
